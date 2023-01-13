80d4569d9b9f28a8cb03486eca5d95bb61763a60
raisinware <raisinware@users.noreply.github.com>
2023-01-13 02:13:11 +0000

    Update Kernel Headers to 1.1.78

--- include/linux/string.h
+++ include/linux/string.h
@@ -11,21 +11,24 @@
 extern "C" {
 #endif
 
+extern char * ___strtok;
 extern char * strcpy(char *,const char *);
 extern char * strncpy(char *,const char *,size_t);
 extern char * strcat(char *, const char *);
 extern char * strncat(char *, const char *, size_t);
-extern char * strchr(const char *,char);
+extern char * strchr(const char *,int);
 extern char * strpbrk(const char *,const char *);
 extern char * strtok(char *,const char *);
+extern char * strstr(const char *,const char *);
 extern size_t strlen(const char *);
 extern size_t strspn(const char *,const char *);
 extern int strcmp(const char *,const char *);
 extern int strncmp(const char *,const char *,size_t);
 
-extern void * memset(void *,char,size_t);
+extern void * memset(void *,int,size_t);
 extern void * memcpy(void *,const void *,size_t);
 extern void * memmove(void *,const void *,size_t);
+extern void * memscan(void *,int,size_t);
 extern int memcmp(const void *,const void *,size_t);
 
 /*
