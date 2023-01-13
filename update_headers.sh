423b4cba41ed4540317700d149b901194480173f
raisinware <102261580+raisinware@users.noreply.github.com>
2023-01-12 17:17:28 -0800

    fetch 1.1.13 headers

--- update_headers.sh
+++ update_headers.sh
@@ -18,11 +18,11 @@ set -e
 
 NAME="linux"
 #VERSION="$(curl -s https://www.kernel.org/ | grep -A1 'mainline:' -m1 | grep -oP '(?<=strong>).*(?=</strong.*)')"
-VERSION="1.0"
+VERSION="1.1.13"
 #shellcheck disable=SC2086
 MVER="$(echo $VERSION | cut -d. -f1)"
 #URL="https://cdn.kernel.org/pub/$NAME/kernel/v$MVER.x/$NAME-$VERSION.tar.xz"
-URL="https://cdn.kernel.org/pub/$NAME/kernel/v$MVER.0/$NAME-$VERSION.tar.xz"
+URL="https://cdn.kernel.org/pub/$NAME/kernel/v$MVER.1/$NAME-$VERSION.tar.xz"
 
 # shellcheck disable=SC2317
 on_exit () {
